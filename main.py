7e93189c08f5a3724e4d9b6b71b3da57e2e2a1f7
Gabriel José Biudes <35238254+gabriel-biudes-dev@users.noreply.github.com>
2022-11-19 02:42:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -91,17 +91,22 @@ def tokenize(code):
     symboltable = {}
     keywordtable = {}
     fulltable = {}
+    breaklines = []
     i1 = 0
     i2 = 0
     i3 = 0
+    count = 0
     keywordlist = ['int', 'float', 'if', 'else', 'for', 'while', '=', '+', '-', '*', '/', '>', '<', '>=', '<=', '==', '(', ')', '{', '}']
     for m in re.finditer(tokens_join, code):
         token_type = m.lastgroup
         token_lexeme = m.group(token_type)
-        if token_type == 'NEWLINE': lin_num += 1
+        if token_type == 'NEWLINE': 
+            lin_num += 1
+            breaklines.append(count)
         elif token_type == 'SKIP': continue
         elif token_type == 'ERRONEOUS': raise RuntimeError('ERRONEOUS: %r inesperado na linha %d' % (token_lexeme, lin_num))
-        else: 
+        else:
+            count = count + 1
             t = Token(token_type, token_lexeme)
             fulltable[i3] = t
             i3 = i3 + 1
@@ -113,16 +118,80 @@ def tokenize(code):
                 if(checkinside(keywordtable.values(), t)): continue
                 keywordtable[i2] = t
                 i2 = i2 + 1
-    return symboltable, keywordtable, fulltable
+    return symboltable, keywordtable, fulltable, breaklines
 
+def getline(i, breaklines):
+    """Gets error line
+
+    Args:
+        i (int): Token index
+        breaklines (int[]): List of breaklines relative to token count
+
+    Returns:
+        int: Error line
+    """
+    for index,x in enumerate(breaklines):
+        if i < x - 1: return index + 1
+
+def checkerrors(fulltable, breaklines):
+    """Gets lexical erros on the code
+
+    Args:
+        fulltable (dict): Dict of tokens
+        breaklines (int[]): List of breaklines relative to token count
+
+    Raises:
+        RuntimeError: Lexical error found
+
+    Returns:
+        bool: Returns False if no lexical error is found
+    """
+    for index,x in enumerate(fulltable):
+        if fulltable[x].token == 'FLOAT' or fulltable[x].token == 'INT':
+            next = fulltable[index + 1].token
+            nextlex = fulltable[index + 1].lexeme
+            if next != 'ID':
+                line = getline(index + 1, breaklines)
+                raise RuntimeError('Erro léxico: %r inesperado na linha %d' % (nextlex, line))
+    return False
+
+def showMenu():
+    """Shows the application menu
+
+    Returns:
+        int: Choosen option
+    """
+    print('\n\n[Lexical Analyzer]')
+    print('\t1)Show symbol table')
+    print('\t2)Show keyword table')
+    print('\t3)Show full table')
+    return int(input('Option: '))
+
+def printdata(symboltable, keywordtable, fulltable, answer):
+    """Show data to the user
+
+    Args:
+        symboltable (dict): Symbol table
+        keywordtable (dict): Keyword table
+        fulltable (dict): Full table
+        answer (int): Choosen option
+    """
+    if answer == 1:
+        for x in symboltable: print(symboltable[x].lexeme, end=" ")
+    if answer == 2:
+        for x in keywordtable: print(keywordtable[x].lexeme, end=" ")
+    if answer == 3:
+        for x in fulltable: print(fulltable[x].token, end=" ")
+        
 def main():
     """Main function
     """
     content = load_file(input('Source file name: '))
-    symboltable, keywordtable, fulltable = tokenize(content)
-    for x in symboltable: print(symboltable[x].lexeme)
-    #for x in fulltable: print(fulltable[x].lexeme)
-    #for x in keywordtable: print(keywordtable[x].lexeme)
-
+    symboltable, keywordtable, fulltable, breaklines = tokenize(content)
+    if checkerrors(fulltable, breaklines): return
+    answer = showMenu()
+    while answer != 4:
+        printdata(symboltable, keywordtable, fulltable, answer)
+        answer = showMenu()
+        
 if __name__ == '__main__': main()
-
